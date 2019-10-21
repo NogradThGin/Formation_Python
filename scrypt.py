0d58079f4b8fbb686f35238cf380dbbea4d74054
NogradThGin <vincent.huriaux@hotmail.fr>
2019-10-21 13:25:24 +0200

    Updating scrypt to Digit Exercise

--- scrypt.py
+++ scrypt.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 value = "adroit#a3#vroom#b52#colorant#e111"
-splited = value.split("#")         
+splited = value.split("#") 
+vrtn = ''
                       
 for i in splited:
-    if i[-1].isdigit():
-        print(i)
-         
+    if not i[-1].isdigit():
+        splited.remove(i)
+        
+print(';'.join(splited))
