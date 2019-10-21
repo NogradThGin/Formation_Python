ae6580e06e30605564ba8e44e667ea93a8e9a502
NogradThGin <vincent.huriaux@hotmail.fr>
2019-10-21 15:33:15 +0200

    Updating scrypt.py to Line'n'Words Counter file

--- scrypt.py
+++ scrypt.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python3
 
-def line_count():  
-    count = int(0)
-    
-    file = open('./example_file', 'r')
-    for i in file.readlines():
-        count += 1
-    
-    file.close()    
-    return count
+#Une cellule morte possédant exactement trois voisines vivantes devient vivante (elle naît).
+#Une cellule vivante possédant deux ou trois voisines vivantes le reste, sinon elle meurt.
 
-def word_count():  
-    count = int(0)
-    
-    file = open('./example_file', 'r')
-    for i in file.readlines():
-        count += len(i.split())
-    
-    file.close()
-    return count
-    
-print("There is {0} lines".format(line_count()))
-print("There is {0} words".format(word_count()))
+#Si une cellule a exactement trois voisines vivantes, elle est vivante à l’étape suivante.
+#Si une cellule a exactement deux voisines vivantes, elle reste dans son état actuel à l’étape suivante.
+#Si une cellule a strictement moins de deux ou strictement plus de trois voisines vivantes, elle est morte à l’étape suivante.
+
