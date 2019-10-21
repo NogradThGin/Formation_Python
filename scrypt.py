52d0ed5acdf13305bb5d044b3cbbc1e406f27455
NogradThGin <vincent.huriaux@hotmail.fr>
2019-10-21 14:16:03 +0200

    Updating scrypt.py to Dictionnary Exercise~

--- scrypt.py
+++ scrypt.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
 
-value = "adroit#a3#vroom#b52#colorant#e111"
-splited = value.split("#") 
-vrtn = ''
-                      
-for i in splited:
-    if not i[-1].isdigit():
-        splited.remove(i)
+def count(string):    
+    dic = {
+            'a': int(0),
+            'e': int(0),
+            'i': int(0),
+            'o': int(0),
+            'u': int(0),
+            'y': int(0),
+    }
         
-print(';'.join(splited))
+    for char in string:
+        if char in dic.keys():
+            dic[char] += 1;
+            
+    return dic
+
+dic = count(input("Enter a sentence to analyze: "))
+
+
+for i in dic:
+    print("Character {0}: {1}".format(i, dic[i]))
